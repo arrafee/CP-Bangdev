be50ab8d83d995a5c0f1ddd3b0cdabe428b18cbc
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2024-10-17 10:12:03 +0700

    slicing services, career, career detail

--- src/components/offering/offering.component.tsx
+++ src/components/offering/offering.component.tsx
@@ -4,6 +4,7 @@ import { Card } from 'flowbite-react';
 import codeLine from '@assets/images/logo/code_line.svg';
 import mobileLine from '@assets/images/logo/mobile_line.svg';
 
+import classNames from 'classnames';
 import style from './offering.module.scss';
 
 const Offering: React.FC = () => {
@@ -14,19 +15,19 @@ const Offering: React.FC = () => {
         <div className={style.stack}>
           <div className={style.card1}>
             <Card>
-              <div className="flex h-[180px] w-[180px] flex-col items-center gap-3">
+              <div className={style.wrapper}>
                 <img src={codeLine} alt="code-line" width="40%" />
-                <span className="text-lg font-semibold">Web Development</span>
-                <p className="text-sm font-extralight text-gray-600">lorem</p>
+                <span className={style.title}>Web Development</span>
+                <p className={classNames([style.description, 'text-gray-600'])}>lorem</p>
               </div>
             </Card>
           </div>
           <div className={style.card2}>
             <Card>
-              <div className="flex h-[180px] w-[180px] flex-col items-center gap-3">
+              <div className={style.wrapper}>
                 <img src={mobileLine} alt="mobile-line" width="40%" />
-                <span className="text-lg font-semibold">Mobile Development</span>
-                <p className="text-sm font-extralight text-gray-600">lorem</p>
+                <span className={style.title}>Mobile Development</span>
+                <p className={classNames([style.description, 'text-gray-600'])}>lorem</p>
               </div>
             </Card>
           </div>
